7bd9ed83a1f94fe54b315caa3b1f6cb9eb57fbb2
hedmon84 <hedmonlopes@gmail.com>
2018-02-05 23:17:25 -0600

    create

--- Kilometraje.cpp
+++ Kilometraje.cpp
@@ -3,4 +3,33 @@
 //
 
 
+#include <iostream>
+using namespace std;
 
+int main() {
+    double  kconducidos = 0;
+    double  Lusados;
+    int salir ;
+    double resultado=0;
+
+
+
+
+    while (kconducidos != -1 ){
+        cout<<"Escriba los kilómetros usados (-1 para salir): ";
+        cin>>kconducidos;
+        cout<<"Escriba los litros: ";
+        cin>>Lusados;
+        double kpl = (kconducidos)/Lusados;
+        cout<<"KPL en este reabastecimiento: "<<kpl<<endl;
+        cout<<"Total KPL: "<< kpl-resultado<<endl<<endl;
+        resultado=kpl;
+
+
+
+    }
+
+
+
+    return 0;
+}
