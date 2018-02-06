2f3840c7a51ae4d48abeef9cf560bca80d811fb3
hedmon84 <hedmonlopes@gmail.com>
2018-02-05 23:48:14 -0600

    create

---
+++ PalindromoInt.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Hedmon lopez on 05/02/2018.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+
+    int numero,p,s,t,c,m;
+
+
+
+    cout<<"ingrese un numero ";
+
+    cin>>numero;
+
+    p=numero%10;
+
+    s=p/10;
+
+    c=s/10;
+
+    m=p*100+t*10+c;
+
+    if (m=numero)
+
+    {
+
+        cout<<"El Numero es Palindromo\n";
+
+    }
+
+    else
+
+    {
+
+        cout<<"El Numero no es Palindromo\n";
+
+    }
+
+
+}
+
